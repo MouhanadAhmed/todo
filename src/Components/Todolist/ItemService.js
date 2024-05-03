f4f66e1d2a198905324cd3a33ea1653c28dc0b23
ahmed.ali <amabdelmongy@gmail.com>
2024-05-03 17:53:57 +0300

    Update ItemService.js

--- src/Components/Todolist/ItemService.js
+++ src/Components/Todolist/ItemService.js
@@ -4,8 +4,8 @@ const API_URL = "http://localhost:8080/api/v1";
 
 export const getItems = async (listId) => {
   try {
-    const response = await axios.get(`${API_URL}/item/${listId}/list`);
-    return response.data.result.Items;
+    return axios.get(`${API_URL}/item/${listId}/list`)
+    .then(response => response.data.result.Items);
   } catch (error) {
     throw new Error(`Error fetching items for list ${listId}: ${error?.response?.data?.message}`);
   }
@@ -13,7 +13,7 @@ export const getItems = async (listId) => {
 
 export const createItem = async (itemName, listId) => {
   try {
-    await axios.post(`${API_URL}/item`, {
+    return axios.post(`${API_URL}/item`, {
       name: itemName,
       list: listId,
       archived: false
@@ -25,9 +25,8 @@ export const createItem = async (itemName, listId) => {
 
 export const updateItemArchivedStatus = async (itemId, archived) => {
   try {
-    await axios.put(`${API_URL}/item/${itemId}`, { archived });
+    return axios.put(`${API_URL}/item/${itemId}`, { archived });
   } catch (error) {
     throw new Error(`Error updating archived status for item ${itemId}: ${error?.response?.data?.message}`);
-
   }
 };
