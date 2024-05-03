84b872d46391de673e4aee7998975ea4f2a0f5a4
ahmed.ali <amabdelmongy@gmail.com>
2024-05-03 12:09:08 +0300

    Create new list

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import Todolist from "../Todolist/Todolist.jsx";
 
 export default function Dashboard() {
   const [lists, setLists] = useState([]);
+  const [AddedNewList, setAddedNewlist] = useState("");
 
   async function fetchLists() {
     try {
@@ -24,9 +25,37 @@ export default function Dashboard() {
     fetchLists();
   }, []);
 
+  const handleKeyPress = async (event) => {
+    if (event.key === "Enter" && setAddedNewlist) {
+      try {
+        await axios.post(`http://localhost:8080/api/v1/list`,
+        {
+          "name": AddedNewList.trim(),
+        });
+        fetchLists();
+        setAddedNewlist("")
+        toast.success(`${AddedNewList} added successfully`);
+      } catch (error) {
+
+        toast.error('Error create new List ' + (error?.response?.data?.message && error.response.data.message));
+
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="container">
-      <h3 className="py-4">Todo</h3>
+      <h4 className="py-4">
+        <i className="fa-solid fa-add me-2"></i>
+        <span>Add New List </span>
+          <input
+              value= {AddedNewList}
+              onChange = {(e) => setAddedNewlist(e.target.value)}
+              onKeyDown = {(e) => handleKeyPress(e, AddedNewList)}
+              type="text"
+            ></input>
+      </h4>
       <div className="row row-cols-1 row-cols-md-2 g-4">
         {lists.map((list) => (
           <div key={list._id} className="col">
